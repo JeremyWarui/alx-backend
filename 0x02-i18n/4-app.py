5340f2505eb99e83c414b41152d194eb80e98bec
JeremyWarui <jerywarne7@gmail.com>
2023-08-02 00:07:52 +0300

    Mock logging in

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -36,7 +36,7 @@ def get_locale():
         if locale in app.config["LANGUAGES"]:
             return locale
     else:
-        return request.accept_language.best_match(app.config["LANGUAGES"])
+        return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 if __name__ == "__main__":
