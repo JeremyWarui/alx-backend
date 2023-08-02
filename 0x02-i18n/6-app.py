56ea1086a07114dee51784722b1fe44ede6b9567
JeremyWarui <jerywarne7@gmail.com>
2023-08-02 06:07:57 +0000

    Use user locale

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -45,7 +45,7 @@ def get_locale():
         return locale
     user = int(requests.args.get("login_as"))
     if user:
-        local_language = users[user]["locale"]
+        local_language = users.user["locale"]
         if local_language in languages:
             return local_language
     req_language = request.headers.get("locale")
